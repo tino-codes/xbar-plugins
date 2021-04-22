e86b0863295e66f5a208bf3df415e02cb5f14fa1
Tino Tischer <tino.tischer@tag24.de>
2021-04-22 11:45:43 +0200

    update readme and descriptions

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
-# xbar-plugins
-Plugins for xbar
+# Plugins for xbar
+
+Get startet here: https://xbarapp.com/
+
+[redmine.1m.php](redmine.1m.php) Show your Redmine tickets, grouped by project, sort by status.
+
+[countee.1m.php](countee.1m.php) Shows free COVID19 vaccination appointments in Saxony's vaccination centers.
--- countee.1m.php
+++ countee.1m.php
@@ -4,11 +4,11 @@
 #  <xbar.version>v1.0</xbar.version>
 #  <xbar.author>Tino</xbar.author>
 #  <xbar.author.github>tino-codes</xbar.author.github>
-#  <xbar.desc>Zeigt freie Impftermine in Sachsen</xbar.desc>
+#  <xbar.desc>Shows free COVID19 vaccination appointments in Saxony's vaccination centers.</xbar.desc>
 #  <xbar.image>https://raw.githubusercontent.com/tino-codes/xbar-plugins/main/countee.png</xbar.image>
 #  <xbar.dependencies>php</xbar.dependencies>
 #  <xbar.abouturl>https://github.com/tino-codes/xbar-plugins</xbar.abouturl>
-#  <xbar.var>select(VAR_IMPFZENTRUM="Dresden"): Das Impfzentrum, welches direkt in der Statusbar angezeigt werden soll.[Annaberg, Belgern, Borna, Chemnitz, Dresden, Eich, Grimma, Kamenz, Dippoldiswalde, Leipzig, Löbau, Mittweida, Pirna, Plauen, Riesa, Zwickau]</xbar.var>
+#  <xbar.var>select(VAR_IMPFZENTRUM="Dresden"): The vaccination center to be displayed directly in the status bar. [Annaberg, Belgern, Borna, Chemnitz, Dresden, Eich, Grimma, Kamenz, Dippoldiswalde, Leipzig, Löbau, Mittweida, Pirna, Plauen, Riesa, Zwickau]</xbar.var>
 
 
 setlocale(LC_TIME, 'de_DE.UTF-8');
