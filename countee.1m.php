28bce894b47d510d17df40482c395c532600d8f5
Tino Tischer <tino.tischer@tag24.de>
2021-04-22 11:36:11 +0200

    add diffrent colors for darkmode, use a select for location selection

--- countee.1m.php
+++ countee.1m.php
@@ -8,12 +8,20 @@
 #  <xbar.image>https://raw.githubusercontent.com/tino-codes/xbar-plugins/main/countee.png</xbar.image>
 #  <xbar.dependencies>php</xbar.dependencies>
 #  <xbar.abouturl>https://github.com/tino-codes/xbar-plugins</xbar.abouturl>
-#  <xbar.var>string(VAR_IZ="Dresden"): Das Impfzentrum, welches direkt in der Statusbar angezeigt werden soll.</xbar.var>
+#  <xbar.var>select(VAR_IMPFZENTRUM="Dresden"): Das Impfzentrum, welches direkt in der Statusbar angezeigt werden soll.[Annaberg, Belgern, Borna, Chemnitz, Dresden, Eich, Grimma, Kamenz, Dippoldiswalde, Leipzig, Löbau, Mittweida, Pirna, Plauen, Riesa, Zwickau]</xbar.var>
+
 
 setlocale(LC_TIME, 'de_DE.UTF-8');
 $dataUrl  = 'https://www.startupuniverse.ch/api/1.1/de/counters/getAll/_iz_sachsen?cached=impfee';
 $json     = file_get_contents($dataUrl);
-$statusIZ = getenv('VAR_IZ');
+$statusIZ = getenv('VAR_IMPFZENTRUM');
+$darkmode = getenv('XBARDarkMode') === 'true';
+
+$green = '#006400';
+
+if ($darkmode) {
+    $green = '#90ee90';
+}
 
 if (!$json) {
     echo '🆘';
@@ -32,7 +40,7 @@ $details = '';
 foreach ($data['response']['data'] as $iz) {
     $details .= $iz['name'] . ': ' . $iz['counteritems'][0]['val'];
     if ($iz['counteritems'][0]['val'] > 0) {
-        $details .= ' | color=#90ee90';
+        $details .= ' | color=' . $green;
     }
     $details .= PHP_EOL;
 
@@ -42,7 +50,7 @@ foreach ($data['response']['data'] as $iz) {
         foreach ($timeslots as $timeslot) {
             $details .= '-- ' . date('d.m.', $timeslot['d']) . ': ' . $timeslot['c'];
             if ($timeslot['c'] > 0) {
-                $details .= ' | color=green';
+                $details .= ' | color=' . $green;
             }
             $details .= PHP_EOL;
         }
@@ -63,3 +71,4 @@ echo $details;
 
 echo '---' . PHP_EOL;
 echo 'Terminvergabe | href=https://sachsen.impfterminvergabe.de/' . PHP_EOL;
+echo 'Countee Übersicht | href=https://www.countee.ch/app/de/counter/impfee/_iz_sachsen' . PHP_EOL;
